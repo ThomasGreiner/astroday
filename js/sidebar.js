8ed2d6e758d4e1fdaa2851dffd69ff22c99a80a9
Thomas Greiner <thomas@greinr.com>
2019-07-27 13:27:49 +0200

    Implemented hamburger menu

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,9 @@
 import {$} from "./utils.js";
 
+$("aside button").addEventListener("click", () => {
+  $("aside").classList.toggle("expanded");
+});
+
 $("aside form").addEventListener("change", (ev) => {
   let input = ev.target;
   document.body.classList.toggle(`setting-${input.name}`, !input.checked);
